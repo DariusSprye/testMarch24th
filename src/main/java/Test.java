5878ae19dac8a06d9432c41b82f0085c0e6abce1
“Darius <dariussprye@gmail.com>
2023-03-24 15:28:03 -0400

    Initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,71 @@
+public class Test {
+
+//    //question 1)
+//    public static void printName(String fullName){
+//        System.out.println(fullName);
+//    }
+//    public static void main(String[] args) {
+//        printName("Darius Sprye");
+//
+//    }
+
+
+
+//    //question 2)
+//    public static void greetUser(String userName){
+//        System.out.println(userName);
+//    }
+//    public static void main(String[] args) {
+//        greetUser("Developers" + " welcome to my website");
+//    }
+
+
+
+    //question 3)
+    /*
+    public static double buyCoffee(double dollars){
+        double remainingBudget = dollars -1;
+        return remainingBudget;
+    }
+    public static void main(String[] args) {
+        double startingBudget = buyCoffee(4);
+        System.out.println("You've spent "  );
+    }
+     */
+
+
+
+
+
+    //question 4)
+    //public static void checkWeather(String weatherCondition, int temperature){
+    
+    //question 5)
+
+    //question 6)
+
+    //question 7)
+
+    //question 8)
+
+    //question 9)
+
+    //question 10)
+
+//    public static void main(String[] args) {
+//
+//        String[][] rapperList = {
+//                {"Biggie", "JayZ", "Nas"},
+//                {"Juicy", "22Twos", "One mic"}
+//        };
+//
+//        for (int i = 0; i < rapperList.length; i++) {
+//            for (int j = 0; j < rapperList[i].length; j++) {
+//                System.out.println(rapperList[i][j]);
+//            }
+//
+//        }
+//
+//    }
+
+}
